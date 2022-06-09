35ed1bf3db10fba02c1cfa37abee94f99565eac3
michele camera <michy.cam95@gmail.com>
2022-06-09 14:54:17 +0200

    modify app component

--- resources/js/app/App.component.vue
+++ resources/js/app/App.component.vue
@@ -3,21 +3,17 @@
 <div>
     ciao come va?
 </div>
-
 </template>
 
 <script>
 export default {
   data() {
     return {
-      count: 0
     }
   }
 }
 </script>
 
 <style scoped>
-button {
-  font-weight: bold;
-}
+
 </style>
