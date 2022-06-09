a3bf7e5b2d2f4f9a6c171d643cd8fabd8fe0aeb1
michele camera <michy.cam95@gmail.com>
2022-06-09 14:53:00 +0200

    created app component

---
+++ resources/js/app/App.component.vue
@@ -0,0 +1,23 @@
+
+<template>
+<div>
+    ciao come va?
+</div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
--- resources/js/frontapp.js
+++ resources/js/frontapp.js
@@ -29,15 +29,18 @@ window.axios.get('http://127.0.0.1:8000/api/posts').then(result => {
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-/**
+
+
+AppComponent = require('./app/App.component.vue').default
+
 const app = new Vue({
     el: '#app',
+    render: (h) => h(AppComponent)
 });
-*/
